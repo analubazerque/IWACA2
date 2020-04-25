63ec2a74b0990721755e9df20bf6ec38654d68e8
Ana Luiza Bazerque <a.bazerque@gmail.com>
2020-04-25 17:45:57 +0000

    fix calculation of selected items

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ var express = require('express'),
     itemCtrl = require('./controllers/item-controller');
     http = require('http'), //This module provides the HTTP server functionalities
     path = require('path'), //The path module provides utilities for working with file and directory paths
-    
+
 
 router.use(express.static(path.resolve(__dirname, 'views'))); //We define the views folder as the one where all content will be served
 
